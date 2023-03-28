f41e8c317aa4d81970dbf6ec8dc76f8174301acf
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-03-28 17:43:13 +0300

    Done with Lab

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = word.split('').reverse().join('');
+  if(reversedWord === word){
+    return true
+  }
+  else if(reversedWord !== word){
+    return false
+  }
 }
 
-/* 
-  Add your pseudocode here
+/*
+  Add your pseudocode here:
+  Invoke functiontion isPalindrome
+  reversed word = reversed method
+  if word equals reversed word
+  return true
+  else if  word doesn't = reversed word
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  The function isPalindrome is invoked and has a parameter of word.
+  In the function a variable, reveresed word, is made.
+  The variable equals to the above parameter.
+  Then the split method turns the variable in to an array.
+  The the reverse method reverses the order of the elements
+  in the parameter array and the join returns the array as a string.
+  Then an if and else if statement checks if the reveresed word variable
+  and the word parameter are the same,
+  and returns true if they are the same or false if they aren't.
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +37,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("kayak"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
